737cc471b22f494b451826616510f0e853dbed93
shiva <shivafullstackdv@gmail.com>
2024-04-03 19:53:09 +0530

    with a basic component

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,164 @@
-import Image from "next/image";
+"use client";
+
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+interface FormData {
+  title: string;
+  subtitle: string;
+  description: string;
+  prerequisites: string;
+  fees: string;
+}
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const addCourseHandle = async (data: FormData) => {
+    console.log("Form submitted");
+    console.log(data);
+    try {
+      const res = await axios.post("http://localhost:5002/courses/add", data);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="page-layout">
+      <h1>Form Submit</h1>
+      <div className="card">
+        <form
+          className="w-full mt-8 mb-8"
+          onSubmit={handleSubmit(addCourseHandle)}
+        >
+          <div className="form-group add-course-form">
+            <div className="form-control w-full flex-row">
+              <input
+                {...register("title", {
+                  required: "Title is required",
+                  minLength: {
+                    value: 15,
+                    message: "Title must be at least 15 characters long",
+                  },
+                  maxLength: {
+                    value: 80,
+                    message: "Title must not be more than 80 characters long",
+                  },
+                })}
+                type="text"
+                id="title"
+                autoComplete="off"
+                placeholder="Enter course title"
+                className="w-5/6 p-1 mb-2"
+              />
+              {errors.title && (
+                <p className="form-error-msg">{errors.title.message}</p>
+              )}
+            </div>
+            <div className="form-control flex-row">
+              <input
+                {...register("subtitle", {
+                  required: "Sub-title is required",
+                  minLength: {
+                    value: 15,
+                    message: "Sub-title must be at least 15 characters long",
+                  },
+                  maxLength: {
+                    value: 80,
+                    message:
+                      "Sub-title must not be more than 80 characters long",
+                  },
+                })}
+                type="text"
+                id="subtitle"
+                autoComplete="off"
+                placeholder="Enter course sub title"
+                className="w-5/6 p-1 mb-2"
+              />
+
+              {errors.subtitle && (
+                <p className="form-error-msg">{errors.subtitle.message}</p>
+              )}
+            </div>
+            <div className="form-control flex-row">
+              <textarea
+                {...register("description", {
+                  required: "Description is required",
+                  minLength: {
+                    value: 200,
+                    message: "Description must be at least 200 characters long",
+                  },
+                  maxLength: {
+                    value: 800,
+                    message:
+                      "Description must not be more than 800 characters long",
+                  },
+                })}
+                id="description"
+                rows={4}
+                placeholder="Enter course description"
+                className="w-5/6 p-1 mb-2 custom-text-area"
+              />
+              {errors.description && (
+                <p className="form-error-msg">{errors.description.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <textarea
+                {...register("prerequisites", {
+                  required: "Description is required",
+                  minLength: {
+                    value: 200,
+                    message: "Description must be at least 200 characters long",
+                  },
+                  maxLength: {
+                    value: 800,
+                    message:
+                      "Description must not be more than 800 characters long",
+                  },
+                })}
+                id="prerequisites"
+                rows={4}
+                placeholder="Enter course prerequisites"
+                className="w-5/6 p-1 mb-2 custom-text-area"
+              />
+              {errors.prerequisites && (
+                <p className="form-error-msg">{errors.prerequisites.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <input
+                {...register("fees", {
+                  required: "Fees is required",
+                  pattern: {
+                    value: /^[0-9]+$/,
+                    message: "Please enter numbers only",
+                  },
+                })}
+                type="number"
+                id="fees"
+                autoComplete="off"
+                placeholder="Enter course fees"
+                className="w-5/6 p-1 mb-2"
+              />
+              {errors.fees && (
+                <p className="form-error-msg">{errors.fees.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <button type="submit" className="btn btn-blue mt-2">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
